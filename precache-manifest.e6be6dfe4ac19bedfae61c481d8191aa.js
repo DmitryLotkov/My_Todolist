98245786caff22dde840602f7eef194e0d142419
DmitryLotkov <dmitry.lotkov9@gmail.com>
2021-11-11 22:23:22 +0300

    Updates

---
+++ precache-manifest.e6be6dfe4ac19bedfae61c481d8191aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fae55a1f80f594248b3498aba36d77",
+    "url": "/HomeworksIncubator/index.html"
+  },
+  {
+    "revision": "ff4e7bb24974080cf64f",
+    "url": "/HomeworksIncubator/static/css/main.0c23c53b.chunk.css"
+  },
+  {
+    "revision": "1e0ea10688d1189cc42a",
+    "url": "/HomeworksIncubator/static/js/2.3067ff63.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/HomeworksIncubator/static/js/2.3067ff63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4e7bb24974080cf64f",
+    "url": "/HomeworksIncubator/static/js/main.8ad69cfb.chunk.js"
+  },
+  {
+    "revision": "cadf402afaad27d87d6e",
+    "url": "/HomeworksIncubator/static/js/runtime-main.cfd8b92b.js"
+  }
+]);
