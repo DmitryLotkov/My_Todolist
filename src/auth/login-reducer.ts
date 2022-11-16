46c2dbb5d8d7abfcddae35fb7a24496f08d6be1a
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-11-16 23:59:10 +0300

    Refactored login-reducer thunks

--- src/auth/login-reducer.ts
+++ src/auth/login-reducer.ts
@@ -1,8 +1,7 @@
-import {Dispatch} from 'redux'
 import {handleServerAppError, handleServerNetworkError} from "../utils/Error-utils";
 import {setAppStatusAC} from "../state/app-reducer";
 import {authAPI, FiledErrorType} from "../api/authAPI";
-import { AxiosError } from "axios";
+import {AxiosError} from "axios";
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 
@@ -14,25 +13,42 @@ export type LoginParamsType = {
 }
 
 // thunks
-export const loginTC = createAsyncThunk<{isLoggedIn: boolean}, LoginParamsType, {
-    rejectValue: {errors: Array<string>, fieldErrors?:Array<FiledErrorType>}
+export const loginTC = createAsyncThunk<undefined, LoginParamsType, {
+    rejectValue: { errors: Array<string>, fieldErrors?: Array<FiledErrorType> }
 }>("auth/login", async (data, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
     try {
         const res = await authAPI.login(data)
         if (res.data.resultCode === 0) {
             thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'})); // Этот AC выключает глобальную крутилку, чтобы показать, что пошел запрос на сервер выполнен
-            return {isLoggedIn: true}; //Этот флаг показывает, что мы залогинились, чтобы был редирект на логин компоненту
+            return
         } else {
             handleServerAppError(res.data, thunkAPI.dispatch);
-            return thunkAPI.rejectWithValue({errors: res.data.messages, fieldErrors:res.data.fieldErrors})
+            return thunkAPI.rejectWithValue({errors: res.data.messages, fieldErrors: res.data.fieldErrors})
         }
-    } catch (err:any) {
+    } catch (err: any) {
         const error: AxiosError = err
         handleServerNetworkError(error, thunkAPI.dispatch);
-        return thunkAPI.rejectWithValue({errors: ["error"], fieldErrors:undefined})
+        return thunkAPI.rejectWithValue({errors: ["error"], fieldErrors: undefined})
     }
 })
+export const logOutTC = createAsyncThunk("auth/logout", async (arg, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+    try {
+        let res = await authAPI.logOut()
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(setIsLoggedInAC({value: false}))
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+            return
+        } else {
+            handleServerAppError(res.data, thunkAPI.dispatch)
+            return thunkAPI.rejectWithValue({})
+        }
+    } catch (error) {
+        handleServerNetworkError(error, thunkAPI.dispatch)
+    }
+})
+
 const slice = createSlice({
     name: "auth",
     initialState: {
@@ -47,9 +63,12 @@ const slice = createSlice({
             state.isInitialized = action.payload.isInitialized
         }
     },
-    extraReducers: builder =>{
-        builder.addCase(loginTC.fulfilled, (state, action)=>{
-            state.isLoggedIn = action.payload.isLoggedIn
+    extraReducers: builder => {
+        builder.addCase(loginTC.fulfilled, (state) => {
+            state.isLoggedIn = true
+        })
+        builder.addCase(logOutTC.fulfilled, (state) => {
+            state.isLoggedIn = false
         })
     }
 })
@@ -60,55 +79,22 @@ export const setIsLoggedInAC = slice.actions.setIsLoggedInAC;
 export const setInitializedAC = slice.actions.setInitializedAC;
 
 
-/*export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC({status: 'loading'})) // Этот AC запускает глобальную крутилку, чтобы показать, что пошел запрос на сервер
+export const initializeAppTC = createAsyncThunk("auth/initialize", async (arg, thunkAPI) => {
+    try {
+        let res = await authAPI.me()
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(setIsLoggedInAC({value: true}));
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}));
+        } else {
+            handleServerAppError(res.data, thunkAPI.dispatch);
+            thunkAPI.dispatch(setAppStatusAC({status: "failed"}));
+        }
+    } catch (error) {
+        handleServerNetworkError(error, thunkAPI.dispatch);
+    } finally {
+            thunkAPI.dispatch(setInitializedAC({isInitialized: true})); // Это надо, чтобы не было бесконечной крутилки. А вообще флаг лечит проблему
+            //передергивания интерфейса с логина на аккаунт
+    }
+})
 
-    authAPI.login(data)
-        .then((res) => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: true})); //Этот флаг показывает, что мы залогинились, чтобы был редирект на логин компоненту
-                dispatch(setAppStatusAC({status: 'succeeded'})); // Этот AC выключает глобальную крутилку, чтобы показать, что пошел запрос на сервер выполнен
-            } else {
-                handleServerAppError(res.data, dispatch);
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch);
-        })
-}*/
-export const logOutTC = () => (dispatch: Dispatch/*<ActionsTasksType | ActionsTodoListsType | AuthActionsType>*/) => {
-    dispatch(setAppStatusAC({status: 'loading'}))
-    authAPI.logOut()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: false}))
-                dispatch(setAppStatusAC({status: 'succeeded'}))
-            } else {
-                handleServerAppError(res.data, dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch)
-        })
-}
 
-export const initializeAppTC = () => (dispatch: Dispatch) => {
-    authAPI.me()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: true}));
-                dispatch(setAppStatusAC({status: 'succeeded'}));
-            } else {
-                /* handleServerAppError(res.data, dispatch);*/
-                dispatch(setAppStatusAC({status: "failed"}));
-            }
-        }).catch((error) => {
-        handleServerNetworkError(error, dispatch);
-    }).finally(() => {
-        dispatch(setInitializedAC({isInitialized: true})); // Это надо, чтобы не было бесконечной крутилки. А вообще флаг лечит проблему
-    })                                                      //передергивания интерфейса с логина на аккаунт
-}
-
-// types
-/*type setIsInitializedType= ReturnType<typeof setInitializedAC>
-export type AuthActionsType = ReturnType<typeof setIsLoggedInAC> | SetAppStatusType | SetAppErrorType | setIsInitializedType*/
